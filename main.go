8b3afefa43b1c7ced28dd0f6e5cf42a5b3224c72
Heber Senger <sengerhe@gmail.com>
2026-06-11 14:29:43 -0300

    next ver

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "text/plain")
-		fmt.Fprintf(w, "OK 3")
+		fmt.Fprintf(w, "OK 4")
 	})
 	http.ListenAndServe(fmt.Sprintf(":%d", *port), mux)
 }
