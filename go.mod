563eee975480d9616409a509dbf506672668352a
Heber Senger <sengerhe@gmail.com>
2026-06-11 05:36:21 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hesenger/pethello
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "port to listen on")
+	flag.Parse()
+
+	log.Printf("Starting server on port %d", *port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "OK")
+	})
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), mux)
+}
